1e1f94cc60a5117987d4b0078b43efd17ef654d7
hvo <hieu.vo@lazada.com>
2017-01-16 11:53:41 +0700

    error handling

--- config.php
+++ config.php
@@ -1,3 +1,47 @@
 <?php
 
-// Insert your configuration parameters here.
+use Tradeshift\interview\Logger;
+
+// create a log channel
+
+
+set_error_handler('errorHandler');
+register_shutdown_function('shutdownHandler');
+
+/**
+ * Shutdown Handler: transforms PHP Fatal Errors into Exceptions
+ *
+ * @throws ErrorException
+ * @return void
+ */
+function shutdownHandler()
+{
+    try {
+        $error = error_get_last();
+        if (isset($error)) {
+            throw new ErrorException($error['message'], 0, $error['type'], $error['file'], $error['line']);
+        }
+    } catch (Exception $e) {
+        Logger::getInstance()->error(var_export($e, true));
+    }
+}
+
+/**
+ * @param int $errno
+ * @param string $errstr
+ * @param string $errfile
+ * @param int $errline
+ */
+function errorHandler($errno, $errstr, $errfile, $errline)
+{
+    $ex = new ErrorException($errstr, 0, $errno, $errfile, $errline);
+
+    Logger::getInstance()->exception(var_export($ex, true), [], [
+        'GET' => $_GET,
+        'POST' => $_POST,
+        'REQUEST' => $_REQUEST,
+        'COOKIE' => $_COOKIE,
+        'SERVER' => $_SERVER
+    ]);
+}
+
---
+++ src/Logger.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LAZHCM10329
+ * Date: 1/16/17
+ * Time: 11:37 AM
+ */
+
+namespace Tradeshift\interview;
+
+use Monolog;
+use Monolog\Handler\StreamHandler;
+
+class Logger
+{
+    protected static $_instance = null;
+    protected $_log = null;
+
+    protected function __construct()
+    {
+        $log = new Monolog\Logger('error');
+        $log->pushHandler(new StreamHandler('logs/error.log', Monolog\Logger::ERROR));
+        $this->_log = $log;
+    }
+
+    /**
+     * @return \Monolog\Logger|null
+     */
+    public function getLogger()
+    {
+        return $this->_log;
+    }
+
+    /**
+     * @param $message
+     * @param array $context
+     * @param array $additionalData
+     */
+    public function error($message, array $context = array(), array $additionalData = [])
+    {
+        $this->log(Monolog\Logger::ERROR, $message, $context, $additionalData);
+    }
+
+    /**
+     * @param $level
+     * @param $message
+     * @param array $context
+     * @param array $additionalData
+     */
+    public function log($level, $message, array $context = array(), array $additionalData = [])
+    {
+        if (!isset($context['additional_data'])) {
+            $context['additional_data'] = [];
+        }
+
+        $context['additional_data'] += $additionalData;
+
+        //$context += $this->getContext();
+
+        if (empty($context['additional_data'])) {
+            $context['additional_data'] = [];
+        }
+
+        //$message = $this->_prepare($message);
+
+        $this->getLogger()->addRecord($level, $message, $context);
+    }
+
+    /**
+     * @return Logger
+     */
+    public static function getInstance()
+    {
+        if (static::$_instance === null) {
+            static::$_instance = new Logger();
+        }
+
+        return static::$_instance;
+    }
+}
